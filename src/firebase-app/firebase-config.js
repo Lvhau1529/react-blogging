28d2fac66482eaa49c76b43e0741cc1a7aa68a6f
lvhau1529 <lvhau1529@gmail.com>
2023-12-16 16:57:02 +0700

    Update features add new

--- src/firebase-app/firebase-config.js
+++ src/firebase-app/firebase-config.js
@@ -1,20 +1,19 @@
-
-import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore/lite';
-import { getAuth } from "firebase/auth";
+import {initializeApp} from 'firebase/app'
+import {getFirestore} from 'firebase/firestore'
+import {getAuth} from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCqBp481PhDnbtdlovMBOGMprmfQs7OEZ4",
-  authDomain: "monkey-blogging-b36aa.firebaseapp.com",
-  projectId: "monkey-blogging-b36aa",
-  storageBucket: "monkey-blogging-b36aa.appspot.com",
-  messagingSenderId: "411708734219",
-  appId: "1:411708734219:web:266d029977397ad26f400e"
-};
+  apiKey: 'AIzaSyCqBp481PhDnbtdlovMBOGMprmfQs7OEZ4',
+  authDomain: 'monkey-blogging-b36aa.firebaseapp.com',
+  projectId: 'monkey-blogging-b36aa',
+  storageBucket: 'monkey-blogging-b36aa.appspot.com',
+  messagingSenderId: '411708734219',
+  appId: '1:411708734219:web:266d029977397ad26f400e'
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+const auth = getAuth(app)
 
-export { app, db, auth }
+export {app, db, auth}
--- src/module/post/PostAddNew.jsx
+++ src/module/post/PostAddNew.jsx
@@ -10,6 +10,21 @@ import DashboardHeading from '../dashboard/DashboardHeading.jsx'
 import {FieldCheckboxes} from '../../components/field'
 import ImageUpload from '../../components/image/ImageUpload'
 import useFirebaseImage from '../../hooks/useFirebaseImage'
+import {Toggle} from '../../components/toggle'
+import {useEffect, useState} from 'react'
+import {Dropdown} from '../../components/dropdown'
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  serverTimestamp,
+  where
+} from 'firebase/firestore'
+import {db} from '../../firebase-app/firebase-config'
+import {toast} from 'react-toastify'
 
 const PostAddNew = () => {
   const {control, watch, setValue, handleSubmit, getValues, reset} = useForm({
@@ -32,11 +47,65 @@ const PostAddNew = () => {
     handleDeleteImage
   } = useFirebaseImage(setValue, getValues)
   const watchStatus = watch('status')
+  const watchHot = watch('hot')
+  const [categories, setCategories] = useState([])
+  const [selectCategory, setSelectCategory] = useState('')
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  useEffect(() => {
+    document.title = 'Monkey Blogging - Add new post'
+  }, [])
+
+  async function getData() {
+    const colRef = collection(db, 'categories')
+    const q = query(colRef, where('status', '==', 1))
+    const querySnapshot = await getDocs(q)
+    let result = []
+    querySnapshot?.forEach((doc) => {
+      result.push({
+        id: doc?.id,
+        ...doc?.data()
+      })
+    })
+    setCategories(result)
+  }
 
   const addPostHandler = async (values) => {
-    values.slug = slugify(values.slug || values.title)
+    const cloneValues = {...values}
+    cloneValues.slug = slugify(values.slug || values.title, {lower: true})
+    cloneValues.status = Number(values.status)
+    const colRef = await collection(db, 'posts')
+    await addDoc(colRef, {
+      ...cloneValues,
+      image,
+      createdAt: serverTimestamp()
+    })
+    toast.success('Create new post successfully!')
+    reset({
+      title: '',
+      slug: '',
+      status: 2,
+      category: {},
+      hot: false,
+      image: '',
+      user: {}
+    })
     handleResetUpload()
+    setSelectCategory({})
     console.log('addPostHandler ~ values:', values)
+  }
+
+  const handleClickOption = async (item) => {
+    const colRef = doc(db, 'categories', item.id)
+    const docData = await getDoc(colRef)
+    setValue('category', {
+      id: docData.id,
+      ...docData.data()
+    })
+    setSelectCategory(item)
   }
 
   return (
@@ -71,8 +140,35 @@ const PostAddNew = () => {
               image={image}
             />
           </Field>
+          <Field>
+            <Label>Category</Label>
+            <Dropdown>
+              <Dropdown.Select placeholder='Select the category'></Dropdown.Select>
+              <Dropdown.List>
+                {categories.length > 0 &&
+                  categories.map((item) => (
+                    <Dropdown.Option
+                      key={item.id}
+                      onClick={() => handleClickOption(item)}>
+                      {item.name}
+                    </Dropdown.Option>
+                  ))}
+              </Dropdown.List>
+            </Dropdown>
+            {selectCategory?.name && (
+              <span className='inline-block p-3 text-sm font-medium text-green-600 rounded-lg bg-green-50'>
+                {selectCategory?.name}
+              </span>
+            )}
+          </Field>
         </div>
         <div className='form-layout'>
+          <Field>
+            <Label>Feature post</Label>
+            <Toggle
+              on={watchHot === true}
+              onClick={() => setValue('hot', !watchHot)}></Toggle>
+          </Field>
           <Field>
             <Label>Status</Label>
             <FieldCheckboxes>
