40e6f515ead5515f71cc3346d2e1ee9b89546a15
lvhau1529 <lvhau1529@gmail.com>
2023-12-17 10:24:15 +0700

    Update rule eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended'
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended'
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
@@ -12,7 +13,28 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': 'warn',
     'react/prop-types': 0,
-    'no-unused-vars': 'warn'
+    'no-unused-vars': 'warn',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before'
+          }
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true
+        }
+      }
+    ],
+    'import/no-unresolved': 'off',
+    'import/order': 'warn'
   },
   ignorePatterns: ['.eslintrc.cjs', 'vite.config.js']
 }
