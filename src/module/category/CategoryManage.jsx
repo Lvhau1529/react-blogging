ef16281e3846121a40551ed97b37347a3d0c277d
lvhau1529 <lvhau1529@gmail.com>
2023-12-17 14:27:13 +0700

    Update category feature

---
+++ src/module/category/CategoryManage.jsx
@@ -0,0 +1,11 @@
+import DashboardHeading from "@/module/dashboard/DashboardHeading"
+
+const CategoryManage = () => {
+  return (
+    <div>
+      <DashboardHeading title='Categories' desc='Manage your category' />
+    </div>
+  )
+}
+
+export default CategoryManage
